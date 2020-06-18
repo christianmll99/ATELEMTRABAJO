7425aa06403521c539c03d6015941586e9d9e7ea
christianmll99 <you@example.com>
2020-06-18 08:55:29 +0200

    Version 7(Aparecen mensajes correctamente pantalla juego)

--- app/src/main/java/es/upv/etsit/att/atelemtrabajo/Pantalla1.java
+++ app/src/main/java/es/upv/etsit/att/atelemtrabajo/Pantalla1.java
@@ -2,6 +2,7 @@ package es.upv.etsit.att.atelemtrabajo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,9 @@ public class Pantalla1 extends AppCompatActivity {
     private TextView tv5;
     protected static String nombrereal = " ";
     protected static String nombrereal2 = " ";
+    boolean muere = false;
+    int contador = 1;
+    int ronda = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,52 +33,71 @@ public class Pantalla1 extends AppCompatActivity {
 
         String nombre11 = getIntent().getStringExtra("nombre1");
         String nombre22 = getIntent().getStringExtra("nombre2");
-         nombrereal = nombre11;
-         nombrereal2 = nombre22;
+        nombrereal = nombre11;
+        nombrereal2 = nombre22;
         tv1.setText(nombre11);
         tv2.setText(nombre22);
 
 
-
     }
 
 
+    public class Pistola {
+        private int posicionDisparo;
+        private int posicionBala;
 
+        public void revolver() {
+            posicionBala = (int) Math.floor(Math.random() * 6 + 1);
+            posicionDisparo = (int) Math.floor(Math.random() * 6 + 1);
+        } //En este método es como si pusieramos las balas y eligieramos la posición del primer tiro
 
-        public void Juego (View v){
-            pistola P = new pistola();
-            int contador = 1;
-            int ronda = 1;
-            do {
-                if (contador % 2 != 0) { //contador impar,actualizamos ronda
-                    tv3.setText(ronda);//mostramos ronda
-                    ronda++; // para la proxima ronda
-                }
-
-                P.disparar();
-                if (contador % 2 != 0) {
-                    // enseñar por pantalla Jugador 1 sobrevive
-                    tv4.setText(nombrereal + " sobrevive ");
-                } else {
-                    //print que jugador 2 sobrevive
-                    tv5.setText(nombrereal2 + " sobrevive ");
-                }
-                contador++;
-
-
-            } while (muere == false);//muere == false
-            if (contador % 2 != 0) {
-                // enseñar por pantalla Jugador 1 muere
-                tv4.setText(nombrereal + " muere");
+        public void actualizaBala() {
+            if (posicionDisparo == 6) {
+                posicionDisparo = 1; //Volvemos a la posición 1 del revolver, sólo tiene 6 espacios para balas
             } else {
-                //print que jugador 2 muere
-                tv5.setText(nombrereal2 + " muere");
+                posicionDisparo++;
             }
-
         }
 
+        public boolean disparar() {
+            //Creamos variable muere
+            if (posicionBala == posicionDisparo) {
+                muere = true; //El jugador muere
+            }
+            actualizaBala(); //Pasamos a la siguiente espacio de bala de la pistola
+            return muere;
+        }
     }
 
 
+    public void Juego(View v) {
+        Pistola P = new Pistola();
+
+        do {
+            if (contador % 2 != 0) { //contador impar,actualizamos ronda
+                tv3.setText(" Ronda : " + ronda);//mostramos ronda
+                ronda++; // para la proxima ronda
+            }
+
+            P.disparar();
+            if (contador % 2 != 0) {
+                // enseñar por pantalla Jugador 1 sobrevive
+                tv4.setText(nombrereal + " sobrevive ");
+            } else {
+                //print que jugador 2 sobrevive
+                tv5.setText(nombrereal2 + " sobrevive ");
+            }
+            contador++;
 
 
+        } while (muere == false);//muere == false
+        if (contador % 2 != 0) {
+            // enseñar por pantalla Jugador 1 muere
+            tv4.setText(nombrereal + " muere");
+        } else {
+            //print que jugador 2 muere
+            tv5.setText(nombrereal2 + " muere");
+        }
+//Meter pagina 3 con resultado final y ganador
+    }
+}
